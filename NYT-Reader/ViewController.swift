d550b720656caade7bdf6c69cfe08aae5f8c6db6
Jeremy Yarley <jeremy.yarley@gmail.com>
2020-06-16 21:38:39 -0400

    Initial Commit

---
+++ NYT-Reader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYT-Reader
+//
+//  Created by Jeremy Yarley on 6/16/20.
+//  Copyright © 2020 Jeremy Yarley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
